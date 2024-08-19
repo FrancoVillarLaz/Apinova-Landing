ff779687c4c5afcba8d330085f985f84263e6bb9
FrancoVillarLaz <villarlazfranco@gmail.com>
2024-08-19 11:59:40 -0300

    primer avance modal

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -1,12 +1,32 @@
 
 
+
+
 function ProjectModal({ isOpen, onClose, title, description, images }) {
   return (
     
-      <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center">
-        <button onClick={onClose} className="mt-4 bg-primary text-white rounded-lg px-4 py-2">
-          Close
-        </button>
+      <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center">
+        <div className="rounded-md flex flex-col w-96 h-96 ">
+          <div className="mt-4">
+            {/* Carrusel de imágenes */}
+
+            
+            <div className="flex overflow-x-scroll">
+              {images.map((image, index) => (
+                <img
+                  key={index}
+                  src={image}
+                  alt={`${title} image ${index + 1}`}
+                  className="h-64 w-full object-cover rounded-lg mr-2"
+                  style={{ flexShrink: 0 }}
+                />
+              ))}
+            </div>
+          </div>
+          <button onClick={onClose} className="mt-4 bg-primary text-white rounded-lg px-4 py-2">
+            Close
+          </button>
+        </div>
       </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}', // Ajusta la ruta según tu proyecto
