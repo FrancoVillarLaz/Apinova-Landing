26a829eb46ea4dc9a994b714098fff8ab3d947f5
FrancoVillarLaz <villarlazfranco@gmail.com>
2024-08-19 21:17:19 -0300

    centrar un div

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="py-6 bg-muted">
+    <footer className="py-6 bg-muted flex justify-center">
       <div className="container px-4 md:px-6 text-center text-muted-foreground">
         <p>&copy; 2024 Apinova. Todos los derechos reservados.</p>
       </div>
