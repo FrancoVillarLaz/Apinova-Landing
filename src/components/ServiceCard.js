857f4f20d1ea86757db73dc88b11a134fbd46070
FrancoVillarLaz <villarlazfranco@gmail.com>
2024-08-19 21:35:26 -0300

    hover servioce card

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ServiceCard({ icon, title, description }) {
   return (
-    <div className="rounded-lg border bg-card text-card-foreground shadow-sm">
+    <div className="rounded-lg border bg-card text-card-foreground shadow-sm transition ease-in-out hover:scale-110">
       <div className="p-6">
         {icon}
         <h3 className="text-lg font-semibold mt-4">{title}</h3>
